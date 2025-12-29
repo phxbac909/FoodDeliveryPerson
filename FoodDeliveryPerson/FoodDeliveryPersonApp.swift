68a36dc00750128668ba9739ad6e4788fb3741c6
ttc <phxbac909@gmail.com>
2025-12-30 04:12:01 +0700

    Initial Commit

---
+++ FoodDeliveryPerson/FoodDeliveryPersonApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDeliveryPersonApp.swift
+//  FoodDeliveryPerson
+//
+//  Created by TTC on 30/12/25.
+//
+
+import SwiftUI
+
+@main
+struct FoodDeliveryPersonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
